945f36f322c320404c39e5b89df091638b4e7f08
Marc Tofan <tofmana@gmail.com>
2022-02-03 22:42:08 +0100

    routes admin orders and user orders checked and fixed

--- database/Users.schema.js
+++ database/Users.schema.js
@@ -37,7 +37,7 @@ const UsersSchema = new Schema(
 
     firstName: {
       type: String,
-      required: false,
+      required: true,
     },
 
     phoneNumber: {
--- models/order.model.js
+++ models/order.model.js
@@ -1,47 +1,74 @@
 import OrdersCollection from "../database/orders.schema";
 
 class Order {
-  constructor(products,user,totalPrice ) {
-    this.products = products,
-    this.user = user,
-    this.totalPrice=totalPrice
+  constructor(products, user, totalPrice) {
+    (this.products = products),
+      (this.user = user),
+      (this.totalPrice = totalPrice);
   }
 
   //Find all orders
-  static getAllOrders (filter = {}) {
-    return OrdersCollection.find();
+  static getAllOrders(filter = {}) {
+    return OrdersCollection.find(filter);
   }
 
-
   //Find an order
-    static findOrder(id) {
-      return OrdersCollection.findById(id);
-    } 
-
-  
+  static findOrder(id) {
+    return OrdersCollection.findById(id);
+  }
 
   //Add an order
   async addOrder() {
     return OrdersCollection.create({
-    products :this.products,
-    user :this.user,
-    totalPrice : this.totalPrice,
+      products: this.products,
+      user: this.user,
+      totalPrice: this.totalPrice,
     });
   }
 
   //UpdateOrder
-  async updateOrder(id){
-    const order = await this.findOrder(id);
-      order.products = this.products,
-      order.user = this.user,
-      order.totalPrice = this.totalPrice,
+  async updateOrder(orderId, userId) {
+    const order = await Order.findOrder(orderId);
+
+    if (order.user.id === userId) {
+      (order.products = this.products),
+        (order.user = this.user),
+        (order.totalPrice = this.totalPrice),
+        await order.save();
+      return;
+    } else {
+      console.log("la");
+      throw new Error("Echec de la mise à jour");
+    }
+  }
+
+  // updateOrderByAdmin
+  async updateOrderByAdmin(orderId) {
+    const order = await Order.findOrder(orderId);
+    if (!order) throw new Error("Introuvable");
+
+    (order.products = this.products),
+      (order.user = this.user),
+      (order.totalPrice = this.totalPrice),
       await order.save();
   }
 
   // DELETE
+  static async deleteOrderById(orderId, userId) {
+    const order = await Order.findOrder(orderId);
+    if (order.user.id === userId) {
+      return OrdersCollection.findByIdAndDelete(orderId);
+    } else {
+      throw new Error("Echec de la suppression");
+    }
+  }
 
-  static deleteOrderById(id) {
-   return  OrdersCollection.findByIdAndDelete(id)
+  // DELETE BY ADMIN
+
+  static async deleteOrderByAdmin(orderId) {
+    const order = await Order.findOrder(orderId);
+    if (!order) throw new Error("Introuvable")
+    return OrdersCollection.findByIdAndDelete(orderId);
   }
 }
 
