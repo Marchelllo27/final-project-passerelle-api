0e69a63762a608a808055ba3080dc88fb41ab7a6
hibabeb <hbb.devpro.com>
2022-02-16 16:18:24 +0100

    update

--- routes/auth-user-routes.js
+++ routes/auth-user-routes.js
@@ -36,7 +36,7 @@ router.get("/auth-user/:id",authUsersControllers.findUserById,
 
 //UPDATE AUTH USER
 router.put(
-  "/update",
+  "/auth-user/update",
   check("email", "E-mail non valide").isEmail(),
   check("password", "Mot de passe invalide")
     .trim()
@@ -91,7 +91,7 @@ router.put(
 
 
 //DELETE AUTH USER FROM DATABASE
-router.delete("/delete", authUsersControllers.deleteUser);
+router.delete("/auth-user/delete", authUsersControllers.deleteUser);
 
 
 export default router;
